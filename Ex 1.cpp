3cb3bafcd4ef349d47ffeb6c8d4ae2ca3eb34be4
rahuam <110572514+rahuam@users.noreply.github.com>
2022-08-03 19:39:27 -0300

    Add files via upload

---
+++ Ex 1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+
+
+int x, y, *p;
+y = 0;
+p = &y;
+x = *p;
+x = 4;
+(*p)++;
+--x;
+(*p) += x;
+printf("x=%d  y=%d *p=%d", x, y, *p);
+}
+
+//resposta: x=3, y=4, *p=4
